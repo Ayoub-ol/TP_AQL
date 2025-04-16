0275b9b67849ccaef2939ac9d314dabad70a6215
Ayoub <ayoubboudiba94@gmail.com>
2025-04-16 16:26:20 +0200

    ex01_TP03_avantComplélerTODO

---
+++ src/main/java/org/example/TP03/Calculatrice.java
@@ -0,0 +1,11 @@
+package org.example.TP03;
+
+public class Calculatrice {
+    private int result ;
+
+    public int additionner(int a, int b) {
+        result = a + b ;
+        return result;
+    }
+
+}
---
+++ src/test/java/org/example/TP03/CalculatriceTest.java
@@ -0,0 +1,42 @@
+package org.example.TP03;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalculatriceTest {
+
+    @Mock
+    private Calculatrice calculatrice;
+
+    @Test
+    public void testAdditionner() {
+        // Définition du comportement de la méthode "additionner"
+        when(calculatrice.additionner(2, 3)).thenReturn(5);
+
+        //TODO : Appel de la méthode à tester
+        // ...
+
+        //TODO : Vérification du résultat
+        // ...
+
+//TODO : Vérification que la méthode "additionner" a été appelée
+// avec les arguments 2 et 3, utiliser la directive « verify »
+
+//TODO : Vérification qu'aucune autre méthode n'a été appelée sur
+//l'objet après l'appel de la méthode "additionner", utiliser la
+// méthode « verifyNoMoreInteractions »
+
+// TODO : Vérification de l'état de l'objet après l'appel de la
+//méthode "additionner", penser à utiliser la méthode
+//« getState() » de la directive « verify » : // exemple :
+        verify(objet).getState()
+    }
+}
